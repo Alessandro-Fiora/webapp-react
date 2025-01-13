5f5f7e55fb7922fefe0e4df6f4304d55c1339456
Alessandro-Fiora <fiora.ale97@gmail.com>
2025-01-13 16:26:37 +0100

    feat: basic page layout with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-// import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
-    <>
-      <h1>React movies</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route index Component={HomePage}></Route>
+          <Route path="/about" Component={AboutPage}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
